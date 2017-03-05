5f55f4a895eb08fe4b89b65c40eebadffcc22414
ToniRib <tonimarierib@gmail.com>
2017-03-05 14:25:02 -0700

    Add seeds file for seeding dev db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,87 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+  def self.start
+    new.generate
+  end
+
+  def generate
+    create_programs
+    create_mods
+    create_program_mods
+    create_lessons
+    schedule_lessons
+    add_recommendations
+    create_voted_lesson_topics
+  end
+
+  def create_programs
+    @back_end = Program.create!(name: 'Back End Engineering', abbreviation: 'BEE')
+    @front_end = Program.create!(name: 'Front End Engineering', abbreviation: 'FEE')
+  end
+
+  def create_mods
+    @mod1 = Mod.create!(name: 'mod 1')
+    @mod2 = Mod.create!(name: 'mod 2')
+    @mod3 = Mod.create!(name: 'mod 3')
+    @mod4 = Mod.create!(name: 'mod 4')
+  end
+
+  def create_program_mods
+    @back_end.mods << Mod.all
+    @front_end.mods << Mod.all
+  end
+
+  def create_lessons
+    @active_record_lesson = Lesson.create!(name: 'ActiveRecord and SQL', description: 'The process of constructing a query, both in raw SQL and ActiveRecord.')
+    @rspec_lesson = Lesson.create!(name: 'Advanced RSpec Testing', description: 'Advanced testing in RSpec, including mocks, stubs, doubles, and testing style.')
+    @git_lesson = Lesson.create!(name: 'Advanced Git', description: 'In depth dive into git, advanced git workflow, and style guides.')
+    @configuring_environment_lesson = Lesson.create!(name: 'Configuring Your Environment', description: 'How to update your terminal prompt, save your GitHub password, and configuring iTerm.')
+    @heroku_lesson = Lesson.create!(name: 'Heroku', description: "A more in depth look at Heroku, how it interacts with git, and what it's capable of.")
+  end
+
+  def schedule_lessons
+    LessonDate.create!(lesson: @active_record_lesson, scheduled_date: Date.new(2016, 5, 2))
+    LessonDate.create!(lesson: @rspec_lesson, scheduled_date: Date.new(2016, 5, 9))
+    LessonDate.create!(lesson: @git_lesson, scheduled_date: Date.new(2016, 5, 16))
+    LessonDate.create!(lesson: @configuring_environment_lesson, scheduled_date: Date.new(2016, 5, 23))
+    LessonDate.create!(lesson: @heroku_lesson, scheduled_date: Date.new(2016, 5, 30))
+  end
+
+  def add_recommendations
+    LessonRecommendation.create!(lesson: @active_record_lesson, program_mod: ProgramMod.find_by(mod_id: @mod2.id, program_id: @back_end.id))
+    LessonRecommendation.create!(lesson: @active_record_lesson, program_mod: ProgramMod.find_by(mod_id: @mod3.id, program_id: @back_end.id))
+    LessonRecommendation.create!(lesson: @active_record_lesson, program_mod: ProgramMod.find_by(mod_id: @mod4.id, program_id: @back_end.id))
+
+    LessonRecommendation.create!(lesson: @rspec_lesson, program_mod: ProgramMod.find_by(mod_id: @mod2.id, program_id: @back_end.id))
+    LessonRecommendation.create!(lesson: @rspec_lesson, program_mod: ProgramMod.find_by(mod_id: @mod3.id, program_id: @back_end.id))
+    LessonRecommendation.create!(lesson: @rspec_lesson, program_mod: ProgramMod.find_by(mod_id: @mod4.id, program_id: @back_end.id))
+
+    LessonRecommendation.create!(lesson: @git_lesson, program_mod: ProgramMod.find_by(mod_id: @mod1.id, program_id: @back_end.id))
+    LessonRecommendation.create!(lesson: @git_lesson, program_mod: ProgramMod.find_by(mod_id: @mod2.id, program_id: @back_end.id))
+    LessonRecommendation.create!(lesson: @git_lesson, program_mod: ProgramMod.find_by(mod_id: @mod3.id, program_id: @back_end.id))
+    LessonRecommendation.create!(lesson: @git_lesson, program_mod: ProgramMod.find_by(mod_id: @mod4.id, program_id: @back_end.id))
+    LessonRecommendation.create!(lesson: @git_lesson, program_mod: ProgramMod.find_by(mod_id: @mod1.id, program_id: @front_end.id))
+    LessonRecommendation.create!(lesson: @git_lesson, program_mod: ProgramMod.find_by(mod_id: @mod2.id, program_id: @front_end.id))
+    LessonRecommendation.create!(lesson: @git_lesson, program_mod: ProgramMod.find_by(mod_id: @mod3.id, program_id: @front_end.id))
+    LessonRecommendation.create!(lesson: @git_lesson, program_mod: ProgramMod.find_by(mod_id: @mod4.id, program_id: @front_end.id))
+
+    LessonRecommendation.create!(lesson: @configuring_environment_lesson, program_mod: ProgramMod.find_by(mod_id: @mod1.id, program_id: @back_end.id))
+    LessonRecommendation.create!(lesson: @configuring_environment_lesson, program_mod: ProgramMod.find_by(mod_id: @mod2.id, program_id: @back_end.id))
+    LessonRecommendation.create!(lesson: @configuring_environment_lesson, program_mod: ProgramMod.find_by(mod_id: @mod1.id, program_id: @front_end.id))
+    LessonRecommendation.create!(lesson: @configuring_environment_lesson, program_mod: ProgramMod.find_by(mod_id: @mod2.id, program_id: @front_end.id))
+
+    LessonRecommendation.create!(lesson: @heroku_lesson, program_mod: ProgramMod.find_by(mod_id: @mod2.id, program_id: @back_end.id))
+    LessonRecommendation.create!(lesson: @heroku_lesson, program_mod: ProgramMod.find_by(mod_id: @mod3.id, program_id: @back_end.id))
+    LessonRecommendation.create!(lesson: @heroku_lesson, program_mod: ProgramMod.find_by(mod_id: @mod4.id, program_id: @back_end.id))
+    LessonRecommendation.create!(lesson: @heroku_lesson, program_mod: ProgramMod.find_by(mod_id: @mod2.id, program_id: @front_end.id))
+    LessonRecommendation.create!(lesson: @heroku_lesson, program_mod: ProgramMod.find_by(mod_id: @mod3.id, program_id: @front_end.id))
+    LessonRecommendation.create!(lesson: @heroku_lesson, program_mod: ProgramMod.find_by(mod_id: @mod4.id, program_id: @front_end.id))
+  end
+
+  def create_voted_lesson_topics
+    VotedLessonTopic.create!(topic: 'Capybara Feature Testing', vote_count: 10)
+    VotedLessonTopic.create!(topic: 'JavaScript ES6', vote_count: 4)
+    VotedLessonTopic.create!(topic: 'Testing React', vote_count: 2)
+  end
+end
+
+Seed.start
