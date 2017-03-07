7a930570349bf96e6a100f4cbf80c0aaacb60652
Toni Rib <tonimarierib@gmail.com>
2017-03-06 20:47:21 -0700

    Add About page content

---
+++ spec/features/guest_viewing_about_page_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+feature 'Guest viewing about page', type: :feature do
+  scenario 'sees a header' do
+    visit about_path
+
+    expect(page).to have_css('h1', text: 'Why do TWT?')
+  end
+end
