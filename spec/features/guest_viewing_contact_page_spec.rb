32d4d57beb5d8489754a0052d66379b3a82e0fc2
Toni Rib <tonimarierib@gmail.com>
2017-03-30 21:15:18 -0600

    Change email address to tuesdayswithtoni.com

--- spec/features/guest_viewing_contact_page_spec.rb
+++ spec/features/guest_viewing_contact_page_spec.rb
@@ -5,7 +5,7 @@ describe 'Guest viewing contact page', type: :feature do
     visit contact_path
 
     expect(page).to have_content 'toni.is.a.verb'
-    expect(page).to have_link 'tonimarierib@gmail.com', href: 'mailto:tonimarierib@gmail.com'
+    expect(page).to have_link 'toni@tuesdayswithtoni.com', href: 'mailto:toni@tuesdayswithtoni.com'
     expect(page).to have_link '@tonimarierib', href: 'https://twitter.com/tonimarierib'
     expect(page).to have_link 'linkedin.com/in/ToniRib', href: 'https://linkedin.com/in/ToniRib'
     expect(page).to have_link 'github.com/ToniRib', href: 'https://github.com/ToniRib'
